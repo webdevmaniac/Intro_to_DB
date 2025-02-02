af09aa924a27ed31bedbc2dd5f992977bc5f8b10
anoldknkomo@gmail.com <anoldknkomo@gmail.com>
2025-02-02 11:20:38 +0200

    Update alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,14 +1,14 @@
-CREATE DATABASE alx_book_store;
+CREATE DATABASE IF NOT EXISTS alx_book_store;
 
 USE alx_book_store;
 
-CREATE TABLE Authors (
+CREATE TABLE IF NOT EXISTS Authors (
   author_id INT AUTO_INCREMENT,
   author_name VARCHAR(215) NOT NULL,
   PRIMARY KEY (author_id)
 );
 
-CREATE TABLE Books (
+CREATE TABLE IF NOT EXISTS Books (
   book_id INT AUTO_INCREMENT,
   title VARCHAR(130) NOT NULL,
   author_id INT NOT NULL,
@@ -18,7 +18,7 @@ CREATE TABLE Books (
   FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
-CREATE TABLE Customers (
+CREATE TABLE IF NOT EXISTS Customers (
   customer_id INT AUTO_INCREMENT,
   customer_name VARCHAR(215) NOT NULL,
   email VARCHAR(215) NOT NULL,
@@ -26,7 +26,7 @@ CREATE TABLE Customers (
   PRIMARY KEY (customer_id)
 );
 
-CREATE TABLE Orders (
+CREATE TABLE IF NOT EXISTS Orders (
   order_id INT AUTO_INCREMENT,
   customer_id INT NOT NULL,
   order_date DATE NOT NULL,
@@ -34,7 +34,7 @@ CREATE TABLE Orders (
   FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
-CREATE TABLE Order_Details (
+CREATE TABLE IF NOT EXISTS Order_Details (
   orderdetailid INT AUTO_INCREMENT,
   order_id INT NOT NULL,
   book_id INT NOT NULL,
